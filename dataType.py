766dc45a86f4e52b9027d2439e89978a31f3bf49
Nandan Singh <nandanatcyber@gmail.com>
2026-07-11 22:47:11 +0530

    updated code

--- dataType.py
+++ dataType.py
@@ -1,6 +1,31 @@
-x = 100
-# to print values 
-print(x)
+#integer data type
+age = 20
+print(age)
 
-print("x:", x)
+print("My age is", age)
 
+
+
+
+#float data type
+age = 20.5
+print(type(age))
+
+#NoneType data type
+x = None
+print(type(x))
+
+#all data types 
+a = 10
+b = 10.5
+c = "Python"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#string data type
+name = "Python"
+print(type(name))
--- demo.py
+++ demo.py
@@ -1,3 +1,88 @@
-x = 10
-y = 30
+Age = 20
+Name = "Nandan"
+Height = 5.9
+
+print("My name is:" ,Name)
+print("My age is:" ,Age)
+print("My height is:" ,Height)
+
+''' this is called dynamic typing , python identifies data types automatically'''
+'''python variable can change their type during execution'''
+
+x= 10
 print(x)
+
+x= "hello"
+print(x)
+
+#python provides a build-in function,type()
+age=20
+print(type(age))
+name="Nandan"
+print(type(name))
+
+#integer data type
+age=20
+temperature=-5
+marks=12
+print(type(age))
+print(type(temperature))
+print(type(marks))
+
+#float storing decimal values
+price=16.5
+weight=0.5
+print(type(price))
+print(type(weight))
+
+#complex number data type- formula: a+bj 
+x=5+3j
+print((x))
+
+#string(str) data type storing text 
+name="nandan"
+city='pune'
+message="""hello world"""
+print(name)
+print(city)
+print(message)
+
+#indexing
+word="python"
+print(word[1])
+
+#boolean data type
+Nandan_is_student=True
+print(Nandan_is_student)
+
+age=18
+print(age<=18)   #18<=18 is true
+age=20
+print(age<20)  #20<20 is false
+
+#list data type stores multiple values in a single variable
+numbers=[20,30,10,50]
+print(numbers)
+
+#tuple
+coordinates= (10,30)
+print(coordinates)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
